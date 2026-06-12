0593837af19f9bf1217d93c99118fc8284432ed9
Ayush Singhal <ay.singhal27@gmail.com>
2026-06-12 18:19:38 +0530

    Update website

--- components/home/ExpertSupport.tsx
+++ components/home/ExpertSupport.tsx
@@ -92,7 +92,7 @@ const cardVariants = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.55, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.55, ease: [0.22, 1, 0.36, 1] as [number, number, number, number]},
   },
 };
 
