a0286233cc1bb531d7af7bba2781d6b9c77f3ac7
CPA DMV <cpa.dmv013@gmail.com>
2026-06-16 17:26:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/website",
-  assetPrefix: "/website/",
   images: {
     unoptimized: true,
   },
